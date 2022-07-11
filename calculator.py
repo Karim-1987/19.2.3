5be15c6d5d03fcbae432b4c3d7e0a42e84b9cb57
Karim-1987 <87887699+Karim-1987@users.noreply.github.com>
2022-07-11 22:52:35 +0300

    task 19.2.3

---
+++ calculator.py
@@ -0,0 +1,14 @@
+
+
+class Calculator:
+    def multiply(self, x, у):
+        return x * у
+
+    def devision(self, x, y):
+        return x / y
+
+    def adding(self, x, y):
+        return x + y
+
+    def subtraction(self, x, y):
+        return x - y
